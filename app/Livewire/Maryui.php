36c37bdaf0fde97c6bfae94fb2a2744ad35b5bca
Wenchez <churio2004@gmail.com>
2025-10-22 14:39:36 -0700

    Added maryui component

---
+++ app/Livewire/Maryui.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Maryui extends Component
+{
+    public function render()
+    {
+        return view('livewire.maryui');
+    }
+}
