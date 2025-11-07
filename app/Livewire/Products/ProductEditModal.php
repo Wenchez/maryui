06b40324bd9a656fbbb07d90673725d8cf79ad06
Wenchez <churio2004@gmail.com>
2025-11-07 11:18:04 -0700

    Creados los modals de crear, borrar y editar producto

---
+++ app/Livewire/Products/ProductEditModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Products;
+
+use Livewire\Component;
+
+class ProductEditModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.products.product-edit-modal');
+    }
+}
