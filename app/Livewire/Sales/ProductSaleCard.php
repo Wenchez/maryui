a66b2aaa6f3b4ce986aafa14ac1306bc4dab3182
Wenchez <churio2004@gmail.com>
2025-11-09 00:10:58 -0700

    Add sales components: implement ProductSaleCard, ProductSaleFilters, ProductsSaleGrid, SaleDetailList, SalePanel, and SaleSummary with corresponding views

---
+++ app/Livewire/Sales/ProductSaleCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Sales;
+
+use Livewire\Component;
+
+class ProductSaleCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.sales.product-sale-card');
+    }
+}
---
+++ app/Livewire/Sales/SalePanel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Sales;
+
+use Livewire\Component;
+
+class SalePanel extends Component
+{
+    public function render()
+    {
+        return view('livewire.sales.sale-panel');
+    }
+}
