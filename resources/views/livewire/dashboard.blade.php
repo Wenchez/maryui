4c176bb3e9b6c45c6a9576b44f32de5a301f5c83
Wenchez <churio2004@gmail.com>
2025-11-16 18:03:06 -0700

    Header al dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,4 +1,5 @@
 <div class="w-full">
+    <x-header title="Panel de control" subtitle="Reportes e información general" separator />
     <livewire:reports.summary-cards />
     <livewire:reports.charts />
     <livewire:reports.lists />
